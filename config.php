612dc51d0e63d647beb7f5c8bdc0427c9d771872
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-09-21 14:57:58 -0400

    Added clean config file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$api_key = # Facebook API Key;
+$secret  = # Facebook API secret Key;
+
+$db_ip = '0.0.0.0';           
+
+$db_user = # Database username;
+$db_pass = # Database user password;
+
+
+$db_name = 'quote';
+
+/*
+create table quotes
+*/
+
+?>
